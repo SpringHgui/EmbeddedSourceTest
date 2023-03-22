e37526a3d7d2eb3b0576e3ec678f9f3a8fdcd006
Gui.H <springhgui@outlook.com>
2023-03-22 14:54:41 +0800

    init

---
+++ EmbeddedSourceTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace EmbeddedSourceTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] manifestResourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            for (int i = 0; i < manifestResourceNames.Length; i++)
+            {
+                Console.WriteLine(manifestResourceNames[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
